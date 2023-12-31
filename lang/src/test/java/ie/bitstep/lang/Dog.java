4d62c0ef56044a5c030fec996aca85e58eef7cb3
John Allen <john.allen@classesarecode.org>
2023-12-31 15:23:01 +0000

    Proxy example

---
+++ lang/src/test/java/ie/bitstep/lang/Dog.java
@@ -0,0 +1,11 @@
+package ie.bitstep.lang;
+
+public class Dog {
+    public void bark() {
+        System.out.println("Bark");
+    }
+
+    public void fetch() {
+        System.out.println("Fetch");
+    }
+}
