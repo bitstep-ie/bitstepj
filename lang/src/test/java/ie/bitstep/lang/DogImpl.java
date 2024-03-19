5569164c40fe090dae6a4ca0c5d3d140d538407d
John Allen <john.allen@classesarecode.org>
2024-03-19 18:42:40 +0000

    Create proxy via declared constructor :)

---
+++ lang/src/test/java/ie/bitstep/lang/DogImpl.java
@@ -0,0 +1,22 @@
+package ie.bitstep.lang;
+
+public class DogImpl implements Dog {
+    public String color;
+
+    public DogImpl() {
+
+    }
+
+    public DogImpl(String color) {
+        this.color = color;
+    }
+
+    public Object bark() {
+        System.out.println("Bark " + color + " dog"); return this;
+    }
+
+    public Object fetch() {
+        System.out.println("Fetch " + color + " dog");
+        return this;
+    }
+}
---
+++ lang/src/test/java/ie/bitstep/lang/EntityProxyTest.java
@@ -0,0 +1,53 @@
+package ie.bitstep.lang;
+
+//import javassist.util.proxy.MethodFilter;
+//import javassist.util.proxy.MethodHandler;
+//import javassist.util.proxy.Proxy;
+//import javassist.util.proxy.ProxyFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Proxy;
+
+public class EntityProxyTest {
+
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        DogImpl dogImpl = new DogImpl("red");
+        Dog dogProxy = (Dog) Proxy.newProxyInstance(
+                EntityProxyTest.class.getClassLoader(),
+                new Class[]{Dog.class},
+                new DynamicEntityHandler(dogImpl));
+
+        Dog dog = (DogImpl) dogProxy.bark();
+    }
+
+        //    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+//        Dog dog = (Dog) viaProxy(Dog.class);
+//        dog.bark();
+//        dog.fetch();
+//    }
+//
+//    private static Object viaProxy(Class<?> clazz) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+//        ProxyFactory f = new ProxyFactory();
+//        f.setSuperclass(clazz);
+//        f.setFilter(new MethodFilter() {
+//            public boolean isHandled(Method m) {
+//                // ignore finalize()
+//                return !m.getName().equals("finalize");
+//            }
+//        });
+//
+//        MethodHandler mi = new MethodHandler() {
+//            public Object invoke(Object object, Method method, Method proxy,
+//                                 Object[] args) throws Throwable {
+//                return proxy.invoke(object, args);  // execute the original method.
+//            }
+//        };
+//
+//        Class<?> proxyCreator = f.createClass();
+//        Constructor<?> constructor = proxyCreator.getConstructor(String.class);
+//        Proxy proxy = (Proxy) constructor.newInstance("red");
+//        proxy.setHandler(mi);
+//
+//        return proxy;
+//    }
+    }
