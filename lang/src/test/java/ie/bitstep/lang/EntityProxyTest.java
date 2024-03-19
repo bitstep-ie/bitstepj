7f2543f0c0b25c011bb94887feebb7251bc54caa
John Allen <john.allen@classesarecode.org>
2024-03-19 18:47:51 +0000

    Create proxy via declared constructor :)

--- lang/src/test/java/ie/bitstep/lang/EntityProxyTest.java
+++ lang/src/test/java/ie/bitstep/lang/EntityProxyTest.java
@@ -11,13 +11,12 @@ import java.lang.reflect.Proxy;
 public class EntityProxyTest {
 
     public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        DogImpl dogImpl = new DogImpl("red");
-        Dog dogProxy = (Dog) Proxy.newProxyInstance(
+        Dog dog = (Dog) Proxy.newProxyInstance(
                 EntityProxyTest.class.getClassLoader(),
                 new Class[]{Dog.class},
-                new DynamicEntityHandler(dogImpl));
+                new DynamicEntityHandler(new DogImpl("red")));
 
-        Dog dog = (DogImpl) dogProxy.bark();
+        dog.bark();
     }
 
         //    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
